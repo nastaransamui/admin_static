f28db126bba09c82704bd62933eae86648b25b25
Majid <nastaran.samui@gmail.com>
2022-07-27 07:03:01 +0700

    fix

--- pages/api/hotels/[id].js
+++ pages/api/hotels/[id].js
@@ -1,27 +1,26 @@
 export default async function handler(req, res) {
   const StreamZip = require('node-stream-zip');
-  const zip = new StreamZip({
-    file: `${process.cwd()}/public/newHotels.zip`,
-    storeEntries: true,
-  });
+
   const { id } = req.query;
-  zip.on('ready', () => {
-    const fileArray = Object.values(zip.entries()).filter(
+  try {
+    const zip = new StreamZip.async({
+      file: `${process.cwd()}/public/newHotels.zip`,
+      storeEntries: true,
+    });
+    const zipObj = await zip.entries();
+    const fileArray = Object.values(zipObj).filter(
       (a) => a.name == `newHotels/${id?.toUpperCase()}/`
     );
     if (fileArray.length > 0) {
-      let zipDotTxtContents = zip
-        .entryDataSync(
-          `newHotels/${id?.toUpperCase()}/${id?.toUpperCase()}_all.json`
-        )
-        .toString('utf8');
-      res
-        .status(200)
-        .json({ success: true, data: JSON.parse(zipDotTxtContents) });
-      zip.close();
+      let zipDotTxtContents = await zip.entryData(
+        `newHotels/${id?.toUpperCase()}/${id?.toUpperCase()}_all.json`
+      );
+      let data = JSON.parse(zipDotTxtContents);
+      res.status(200).json({ success: true, data: data });
     } else {
-      res.status(400).json({ success: false, data: [] });
-      zip.close();
+      res.status(400).json({ success: true, data: [] });
     }
-  });
+  } catch (error) {
+    res.status(400).json({ success: false, Error: error.toString() });
+  }
 }
