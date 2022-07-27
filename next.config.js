10c9e9a71fa2ba3f245d95f9188b9a8f57f0bd98
Majid <nastaran.samui@gmail.com>
2022-07-27 07:09:14 +0700

    update nexconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+// Tell Next.js these files will be read at runtime by the below code:
+const path = require('path');
+path.resolve('./public/');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
