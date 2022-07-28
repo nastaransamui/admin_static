873c783e2d40689d8132fc3728bcdddac6cb38c1
Majid <nastaran.samui@gmail.com>
2022-07-28 10:54:09 +0700

    api list query

--- pages/api/hotels/list.js
+++ pages/api/hotels/list.js
@@ -2,9 +2,29 @@
 import fs from 'fs';
 import path from 'path';
 
+export function escapeRegExp(value) {
+  return value.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+}
+
 export default async function handler(req, res) {
   const hotelJson = path.join(process.cwd(), 'public', 'hotelsCountry.json');
   const hotelsFolderArray = fs.readFileSync(hotelJson);
   const hotelData = JSON.parse(hotelsFolderArray);
-  res.status(200).json({ data: hotelData });
+  if (Object.keys(req.query).length === 0) {
+    res.status(200).json({ data: hotelData });
+  } else {
+    const filterValue = Object.values(req.query)[0];
+    const fieldValue = Object.keys(req.query)[0];
+    const searchRegex = new RegExp(escapeRegExp(filterValue), 'i');
+    var filteredData = hotelData.filter((row) => {
+      return Object.keys(row).some((field) => {
+        if (field == fieldValue) {
+          if (row[field] !== null) {
+            return searchRegex.test(row[field].toString());
+          }
+        }
+      });
+    });
+    res.status(200).json({ data: filteredData });
+  }
 }
